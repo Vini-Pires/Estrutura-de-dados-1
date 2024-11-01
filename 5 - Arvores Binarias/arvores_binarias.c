c821c2e4f1bb4675478206e43c161a2e996c135e
Vinicius Pires | UTFPR <vinciuspires@alunos.utfpr.edu.br>
2024-11-01 09:11:18 -0300

    aula 01/11
    tarefa para fazer comentada

--- 5 - Arvores Binarias/arvores_binarias.c
+++ 5 - Arvores Binarias/arvores_binarias.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct objeto {
-  int cpf;
-  int idade;
-  char nome[30];
-  // demais infos
-  struct objeto *esq;
-  struct objeto *dir;
-} OBJETO;
+typedef struct noArvore {
+  int chave;
+  // demais infos para representar o objeto
+  struct noArvore *esq;
+  struct noArvore *dir;
+} NO_ARVORE;
+
+typedef struct arvore {
+  NO_ARVORE *raiz;
+} ARVORE;
+
+/*
+[Tarefa]
+  Faça os elementos abaixo
+    - inicializar
+    - estaVazio
+*/
+void inicializar();
+
+int main (void) {
+  return EXIT_SUCCESS;
+}
+
+void inicializar() {}
